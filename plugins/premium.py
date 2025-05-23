aae18b79b8804d44e197d2f37cc77afbc88bc9d7
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-23 12:24:28 +0530

    Add files via upload

--- plugins/premium.py
+++ plugins/premium.py
@@ -4,7 +4,7 @@ import datetime, time
 from Script import script 
 from config import ADMINS, LOG_CHANNEL
 from utils import get_seconds
-from database.db_users import db 
+from plugins.dbusers import db 
 from pyrogram import Client, filters 
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
